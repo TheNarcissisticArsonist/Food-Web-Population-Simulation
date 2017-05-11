3345304580b8b5ae0022e526e67b28cb106aa159
Tommy <TommyCohn116@gmail.com>
2017-05-11 09:51:52 -0400

    Added code for mouseMoved function.

--- script.js
+++ script.js
@@ -2,12 +2,21 @@
 // Constants
 //------------------------------------------------------------
 
+var defaultZoom = 1; //The default zoom level. Zoom is given in pixels per graph unit. The graph is done in units of years and number of organisms.
+var dragPanningConstant = 1/defaultZoom; //This constant slows down the rate that dragging pans the graph.
+
 //------------------------------------------------------------
 // Global Variables
 //------------------------------------------------------------
 
 var page = {}; //An object containing the html elements for everything on the page you need to interact with.
 var ctx; //The canvas' 2D context.
+var mouseLocation = []; //Used for the mouseMoved function. This is the current mouse position.
+var oldMouseLocation = []; //And this is the previous mouse position.
+var mouseButtons = {}; //An object containing the states of mouse buttons.
+var overCanvas; //Whether or not the cursor is over the canvas.
+var pos = []; //The position on the graph of the location of the center of the graph window.
+var zoom; //The zoom level of the graph display.
 
 //------------------------------------------------------------
 // Classes
@@ -47,7 +56,19 @@ function userInputSetup() {
 	page.numOrgs.addEventListener("change", updateNumOrgs);
 }
 function mouseMoved(e) {
-	//
+	oldMouseLocation[0] = mouseLocation[0];
+	oldMouseLocation[1] = mouseLocation[1];
+	mouseLocation[0] = event.clientX;
+	mouseLocation[1] = event.clientY;
+
+	if(mouseButtons["1"] && overCanvas) {
+		var delta = [0, 0];
+		delta[0] = mouseLocation[0]-oldMouseLocation[0];
+		delta[1] = mouseLocation[1]-oldMouseLocation[1];
+
+		pos[0] += -1 * delta[0] * dragPanningConstant * defaultZoom * (1/zoom);
+		pos[1] += delta[1] * dragPanningConstant * defaultZoom * (1/zoom);
+	}
 }
 function mouseDown(e) {
 	//
@@ -81,3 +102,5 @@ function updateNumOrgs() {
 //------------------------------------------------------------
 // Executed Code
 //------------------------------------------------------------
+
+setup();
